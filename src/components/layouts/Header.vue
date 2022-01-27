be8f51f4231bd006073d130d23d6f719fc523a91
Ahamed Arshad <achchu.zats@gmail.com>
2022-01-27 17:37:53 +0530

    Added: basic layout with header and hero

---
+++ src/components/layouts/Header.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="relative bg-white">
+		<div class="max-w-7xl mx-auto px-4 sm:px-6">
+			<div class="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
+				<div class="flex justify-start lg:w-0 lg:flex-1">
+					<h2 class="text-xl font-bold">OrderSpark</h2>
+				</div>
+				<div class="md:flex items-center justify-end md:flex-1 lg:w-0">
+					<button type="button" class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+						<span class="sr-only">cart</span>
+						<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+							<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+						</svg>
+					</button>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
